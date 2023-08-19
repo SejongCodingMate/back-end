40d797405a7f63bc0630dee6e12f534356403440
이승렬 <ibban9810@icloud.com>
2023-08-19 19:42:19 +0900

    Feat: SpringSecurity 초기설정 완료

---
+++ src/main/java/com/creativesemester/SejongCodingMate/global/security/UserDetailsServiceImpl.java
@@ -0,0 +1,24 @@
+package com.creativesemester.SejongCodingMate.global.security;
+
+import com.creativesemester.SejongCodingMate.member.entity.Member;
+import com.creativesemester.SejongCodingMate.member.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private final MemberRepository memberRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String studentId) {
+        Member member = memberRepository.findByStudentId(studentId)
+                .orElseThrow(() -> new UsernameNotFoundException("사용자를 찾을수 없습니다."));
+
+        return new UserDetailsImpl(member, member.getStudentId());
+    }
+}
