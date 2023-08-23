7963048297376dc3e554b58260af6397dd9065d4
dmdwn99 <98579376+dmdwn99@users.noreply.github.com>
2023-08-23 11:57:56 +0900

    Feat: Member Logic 구현 1. 회원가입 기능 구현 2. 비밀변호 변경 기능 구현 3. 로그인 기능 구현

--- src/main/java/com/creativesemester/SejongCodingMate/domain/member/controller/MemberController.java
+++ src/main/java/com/creativesemester/SejongCodingMate/domain/member/controller/MemberController.java
@@ -1,10 +1,16 @@
 package com.creativesemester.SejongCodingMate.domain.member.controller;
 
 
-import com.creativesemester.SejongCodingMate.domain.member.dto.request.SignUpRequest;
+import com.creativesemester.SejongCodingMate.domain.member.dto.request.AuthRequestDto;
+import com.creativesemester.SejongCodingMate.domain.member.dto.response.AuthResponseDto;
+import com.creativesemester.SejongCodingMate.domain.member.service.AuthService;
 import com.creativesemester.SejongCodingMate.domain.member.service.MemberService;
+import com.creativesemester.SejongCodingMate.global.response.GlobalResponseDto;
+import com.creativesemester.SejongCodingMate.global.response.ResponseCode;
+import com.google.gson.JsonObject;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,18 +24,31 @@ public class MemberController {
 
 
     private final MemberService memberService;
+    private final AuthService authService;
 
     @PostMapping("/api/member/sign-up")
-    public ResponseEntity <Object> signUp (@RequestBody @Valid SignUpRequest signUpRequest){
-        return ResponseEntity.ok(memberService.signUp(signUpRequest));
+    public ResponseEntity <GlobalResponseDto> signUp (@RequestBody @Valid AuthRequestDto authRequestDto){
+        return memberService.signUp(authRequestDto);
     }
 
     @PostMapping("/api/member/login")
-    public ResponseEntity <Object> login (@RequestBody @Valid SignUpRequest signUpRequest, HttpServletResponse response){
-        return memberService.login(signUpRequest,response);
+    public ResponseEntity <GlobalResponseDto> login (@RequestBody @Valid AuthRequestDto authRequestDto, HttpServletResponse response){
+        return memberService.login(authRequestDto,response);
     }
 
+    @PostMapping("/api/member/sejong-login")
+    public ResponseEntity <GlobalResponseDto> authenticateSejongStudent (@RequestBody @Valid AuthRequestDto authRequestDto) {
+        JsonObject body = authService.authenticateSejongStudent(authRequestDto);
+        if (body == null) {
+            return ResponseEntity.ok(GlobalResponseDto.of(ResponseCode.SEJONG_AUTHENTICATION_FAILED));
+        } else {
+            return ResponseEntity.ok(GlobalResponseDto.of(ResponseCode.SEJONG_AUTHENTICATION,
+                    new AuthResponseDto(authRequestDto.getId())));
+        }
+    }
 
-
-
+    @PatchMapping("/api/member/password")
+    public ResponseEntity <GlobalResponseDto> changePassword (@RequestBody @Valid AuthRequestDto authRequestDto) {
+        return memberService.changePassword(authRequestDto);
+    }
 }
