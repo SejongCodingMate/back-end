acb5a6baea7d4dab871c598669d001052b7d499c
dmdwn99 <98579376+dmdwn99@users.noreply.github.com>
2023-09-03 19:58:32 +0900

    Feat: Code 엔티티 구성 1. Code 엔티티 구현 2. Code 실행 후 저장

---
+++ src/main/java/com/creativesemester/SejongCodingMate/domain/code/dto/CodeRequestDto.java
@@ -0,0 +1,16 @@
+package com.creativesemester.SejongCodingMate.domain.code.dto;
+
+import lombok.Getter;
+
+import javax.validation.constraints.NotBlank;
+
+@Getter
+public class CodeRequestDto {
+
+    @NotBlank(message = "코드를 입력해주세요.")
+    private String code;
+
+    @NotBlank(message = "문제 번호를 입력해주세요")
+    private Long problemId;
+
+}
